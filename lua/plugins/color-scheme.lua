c3557afcaad20bdec632a02cd494f6f5e899b0cb
desmoscook <desmoscook@163.com>
2024-11-25 14:20:54 +0800

    add color-scheme config

--- lua/plugins/color-scheme.lua
+++ lua/plugins/color-scheme.lua
@@ -1,12 +1,18 @@
 return {
-  --   -- add gruvbox
-  --   { "ellisonleao/gruvbox.nvim" },
-  --
-  --   -- Configure LazyVim to load gruvbox
-  --   {
-  --     "LazyVim/LazyVim",
-  --     opts = {
-  --       colorscheme = "gruvbox",
-  --     },
-  --   },
+  { "sainnhe/gruvbox-material" },
+  { "olimorris/onedarkpro.nvim" },
+  { "navarasu/onedark.nvim" },
+  { "navarasu/onedark.nvim" },
+  { "Mofiqul/dracula.nvim" },
+  { "folke/tokyonight.nvim" },
+  { "rose-pine/neovim" },
+  { "marko-cerovac/material.nvim" },
+  { "rose-pine/neovim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
 }
